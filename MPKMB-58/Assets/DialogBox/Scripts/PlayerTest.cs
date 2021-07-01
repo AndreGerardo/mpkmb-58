2794f536e89f242fa2344d53cfc8fefee38dab72
arha7777m <iarhamirzaibrahim@apps.ipb.ac.id>
2021-07-02 06:06:14 +0700

    Done update tutorial

---
+++ MPKMB-58/Assets/DialogBox/Scripts/PlayerTest.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTest : MonoBehaviour
+{
+    [SerializeField] private DialogueUI dialogueUI;
+
+    private const float MovSpd = 10f;
+
+    public DialogueUI DialogueUI => dialogueUI;
+
+    public IInteractable Interactable { get; set; }
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        //biar player gk gerak pas dialogbox muncul
+        if (dialogueUI.IsOpen) return;
+
+        //Input gerak
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        rb.MovePosition(rb.position + input.normalized * (MovSpd * Time.fixedDeltaTime));
+
+        if (Input.GetKeyDown(KeyCode.E) || Input.GetMouseButtonDown(0))
+        {
+            Interactable?.Interact(this);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

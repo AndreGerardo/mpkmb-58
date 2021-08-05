2fb42a7733cee5a3d245de98a54005cf1dd946f0
AndreGerardo <andregerardo21@gmail.com>
2021-08-05 17:10:45 +0700

    MultiScenemanagement

--- MPKMB-58/Assets/Scripts/SceneMovement/SceneMovement.cs
+++ MPKMB-58/Assets/Scripts/SceneMovement/SceneMovement.cs
@@ -4,19 +4,15 @@ using UnityEngine;
 
 public class SceneMovement : Item
 {
-    public Transform player;
+    private MultiSceneManager multiSceneManager;
+    private Transform player;
     public Transform designatedPos;
-    [Header("Change Parameters")]
-    public int BACKGROUND_NUM = 1; //Background 1,2 atau,3
-    public List<Sprite> backgrounds;
-    public List<GameObject> sceneButtons;
-    public List<GameObject> backgroundCollections;
     public bool toTheRight = true;
-    private SpriteRenderer background;
 
-    void Start()
+    private void Start()
     {
-        background = GameObject.Find("Background").GetComponent<SpriteRenderer>();
+        multiSceneManager = GetComponentInParent<MultiSceneManager>();
+        player = GameObject.Find("Player").GetComponent<Transform>();
     }
 
     public override void Interact()
@@ -31,43 +27,16 @@ public class SceneMovement : Item
             GoLeft();
         }
 
-        if(BACKGROUND_NUM != backgrounds.Count && BACKGROUND_NUM != 1)
-        {
-            sceneButtons[0].SetActive(true);
-            sceneButtons[1].SetActive(true);
-        }else if(BACKGROUND_NUM == 1)
-        {
-            sceneButtons[0].SetActive(true);
-            sceneButtons[1].SetActive(false);
-        }else if(BACKGROUND_NUM == backgrounds.Count)
-        {
-            sceneButtons[0].SetActive(false);
-            sceneButtons[1].SetActive(true);
-        }
     }
 
     private void GoRight()
     {
-        background.sprite = backgrounds[BACKGROUND_NUM];
-
-        foreach (var items in backgroundCollections)
-        {
-            items.SetActive(false);
-        }
-
-        backgroundCollections[BACKGROUND_NUM].SetActive(true);
+        multiSceneManager.BACKGROUND_NUM++;
     }
 
     private void GoLeft()
     {
-        background.sprite = backgrounds[BACKGROUND_NUM-2];
-
-        foreach (var items in backgroundCollections)
-        {
-            items.SetActive(false);
-        }
-
-        backgroundCollections[BACKGROUND_NUM-2].SetActive(true);
+        multiSceneManager.BACKGROUND_NUM--;
     }
 
 }

f8a5f470db569b1969173647fec74a070b850c30
fathiannaufal <fathi_annaufal@apps.ipb.ac.id>
2021-08-09 20:09:17 +0700

    push macem macem

---
+++ MPKMB-58/Assets/Scripts/Save Game/SaveGame.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveGame : MonoBehaviour
+{
+    [SerializeField] private int thisChapter;
+    public int chapter;
+
+    [Header("Configuration")]
+    [SerializeField] private SaveScriptableObject _saveScriptableObject = default;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().interactable = false;
+        chapter = _saveScriptableObject.Chapter;
+        CheckChapter();
+    }
+    
+    public void CheckChapter()
+    {
+        Debug.Log("chapter checked");
+        if(thisChapter <= chapter)
+        {
+            GetComponent<Button>().interactable = true;
+        }
+    }
+    
+}
---
+++ MPKMB-58/Assets/Scripts/Scriptable Object/SaveScriptableObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Save/Saved Game")]
+public class SaveScriptableObject : ScriptableObject
+{
+    public int Chapter;
+
+    void SaveGame(int currentChapter)
+    {
+        Chapter = currentChapter;
+    }
+}

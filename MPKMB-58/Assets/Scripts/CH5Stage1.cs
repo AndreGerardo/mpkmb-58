cff53a73389c1ddcaaf9da2def459bdb07ff38a0
AndreGerardo <andregerardo21@gmail.com>
2021-08-10 17:39:54 +0700

    Finishing

---
+++ MPKMB-58/Assets/Scripts/CH5Stage1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CH5Stage1 : MonoBehaviour
+{
+    public NPCAdvance teknisi;
+    private bool trigger = true;
+
+    void Update()
+    {
+        if(teknisi.isDone && trigger)
+        {
+            StartCoroutine(nextDelay());
+            trigger = false;
+        }
+        
+    }
+
+    IEnumerator nextDelay()
+    {
+        yield return new WaitForSeconds(0.5f);
+        GetComponent<SceneManagement>().NextScene();
+    }
+}

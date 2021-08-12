9318416085c7566409ec44ddf1a316e91a730a62
fathiannaufal <fathi_annaufal@apps.ipb.ac.id>
2021-08-12 09:24:36 +0700

    rapihin

---
+++ MPKMB-58/Assets/Scripts/Save Game/DeveloperDebug.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeveloperDebug : MonoBehaviour
+{
+    public void ResetGame()
+    {
+        PlayerPrefs.SetInt("CH", 1);
+        Debug.LogWarning("chapter reset to : " + PlayerPrefs.GetInt("CH"));
+
+    }
+}
